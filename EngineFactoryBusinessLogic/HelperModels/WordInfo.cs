0e7c8a5a9534be94448c6d75c97bfd478dbd83f3
Михаил Бузаров <tumbashka@gmail.com>
2020-03-20 09:17:38 +0400

    a

---
+++ EngineFactoryBusinessLogic/HelperModels/WordInfo.cs
@@ -0,0 +1,14 @@
+﻿using EngineFactoryBusinessLogic.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineFactoryBusinessLogic.HelperModels
+{
+    class WordInfo
+    {
+        public string FileName { get; set; }
+        public string Title { get; set; }
+        public List<DetailViewModel> Details { get; set; }
+    }
+}
---
+++ EngineFactoryBusinessLogic/HelperModels/WordParagraphProperties.cs
@@ -0,0 +1,11 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace EngineFactoryBusinessLogic.HelperModels
+{
+    class WordParagraphProperties
+    {
+        public string Size { get; set; }
+        public bool Bold { get; set; }
+        public JustificationValues JustificationValues { get; set; }
+    }
+}
