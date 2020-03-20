2a35e00ce6c0742798bb9bde0b6cf8e26e7d9578
Михаил Бузаров <tumbashka@gmail.com>
2020-03-20 08:53:38 +0400

    a1

---
+++ EngineFactoryBusinessLogic/ViewModels/ReportOrdersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using EngineFactoryBusinessLogic.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineFactoryBusinessLogic.ViewModels
+{
+    public class ReportOrdersViewModel
+    {
+        public DateTime DateCreate { get; set; }
+        public string EngineName { get; set; }
+        public int Count { get; set; }
+        public decimal Sum { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
---
+++ EngineFactoryBusinessLogic/ViewModels/ReportProductComponentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineFactoryBusinessLogic.ViewModels
+{
+    public class ReportEngineDetailViewModel
+    {
+        public string DetailName { get; set; }
+        public int TotalCount { get; set; }
+        public List<Tuple<string, int>> Engines { get; set; }
+    }
+}
