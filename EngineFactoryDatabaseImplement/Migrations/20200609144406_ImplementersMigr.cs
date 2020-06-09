e57fe2a61d3f3ed30c7b27bf8ae2bc279378c57e
Михаил Бузаров <tumbashka@gmail.com>
2020-06-09 18:48:01 +0400

    fix

---
+++ EngineFactoryDatabaseImplement/Migrations/20200609144406_ImplementersMigr.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EngineFactoryDatabaseImplement.Migrations
+{
+    public partial class ImplementersMigr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
