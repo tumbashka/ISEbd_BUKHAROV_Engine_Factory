7829043e3e81b65c92a6c5dda92be472edf3235c
Михаил Бузаров <tumbashka@gmail.com>
2020-06-24 22:10:17 +0400

    fix

--- EngineFactoryClientView/FormMain.cs
+++ EngineFactoryClientView/FormMain.cs
@@ -20,13 +20,20 @@ namespace EngineFactoryClientView
         {
             try
             {
-                dataGridView.DataSource = APIClient.GetRequest<List<OrderViewModel>>($"api/main/getorders?clientId={Program.Client.Id}");
+                dataGridView.DataSource = APIClient.GetRequest<List<OrderViewModel>>($"api/main/getorders?clientId={Program.Client.Id}");           
                 dataGridView.Columns[0].Visible = false;
                 dataGridView.Columns[1].Visible = false;
                 dataGridView.Columns[2].Visible = false;
                 dataGridView.Columns[3].Visible = false;
-                dataGridView.Columns[3].AutoSizeMode =
-               DataGridViewAutoSizeColumnMode.Fill;
+                dataGridView.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                dataGridView.Columns[4].HeaderText = "Название двигателя";
+                dataGridView.Columns[5].HeaderText = "ФИО исполнителя";
+                dataGridView.Columns[6].HeaderText = "Кол-во";
+                dataGridView.Columns[7].HeaderText = "Сумма";
+                dataGridView.Columns[8].HeaderText = "Статус";
+                dataGridView.Columns[9].HeaderText = "Дата создания";
+                dataGridView.Columns[10].HeaderText = "Дата выполнения";
+                dataGridView.Columns[11].Visible = false;
             }
             catch (Exception ex)
             {
